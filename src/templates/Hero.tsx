0085bf923cfe631df3824ab7011456732b03a82c
vonintsoa <rhjmanda@gmail.com>
2025-09-25 10:14:31 +0300

    changement style hero

--- src/templates/Hero.tsx
+++ src/templates/Hero.tsx
@@ -10,7 +10,7 @@ export function Hero(){
          <h6 className="text-warning">Bonjour, je suis</h6>
          <div className={`hero__title`}>
             <div className={`uppercase fade ${observerClass}`} ref={ref}>RAHAJANIRINA</div> 
-            <div className={`uppercase fade ${observerClass}`} ref={ref}  style={{transitionDelay: ".1s"}}> Mandaniaina<span className="text-primary"> Vonintsoa</span> <br></br>Fitiavana</div>
+            <div className={`uppercase fade ${observerClass}`} ref={ref}  style={{transitionDelay: ".1s"}}> Mandaniaina<span className="text-primary"> Vonintsoa</span></div>
          </div>
          <div className={`hero__subtitle fade ${observerClass}`} style={{transitionDelay: ".2s"}}>Développeur <u>Symfony</u> / <u>React</u></div>
          {/* <p className="mt-3">Développeur Symfony & React, passionné par la création d’applications<br></br> web modernes et performantes</p> */}
