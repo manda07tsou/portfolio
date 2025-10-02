f05e8de34d4d82d37890c3373b951c1b35c3cc60
vonintsoa <rhjmanda@gmail.com>
2025-10-02 15:49:47 +0300

    changement couleur primary

--- src/templates/Hero.tsx
+++ src/templates/Hero.tsx
@@ -7,12 +7,12 @@ export function Hero(){
 
    return <section className="section hero" id="hero">
       <div className="hero__body">
-         <h6 className="text-warning">Bonjour, je suis</h6>
+         <h6 className="text-primary">Bonjour, je suis</h6>
          <div className={`hero__title`}>
             <div className={`uppercase fade ${observerClass}`} ref={ref}>RAHAJANIRINA</div> 
-            <div className={`fade ${observerClass}`} ref={ref}  style={{transitionDelay: ".1s"}}> Mandaniaina<span className="text-primary"> Vonintsoa</span></div>
+            <div className={`fade ${observerClass}`} ref={ref}  style={{transitionDelay: ".1s"}}> Mandaniaina<span className="text"> Vonintsoa</span></div>
          </div>
-         <div className={`hero__subtitle fade ${observerClass}`} style={{transitionDelay: ".2s"}}>Développeur Symfony / React</div>
+         <div className={`hero__subtitle fade text-800 ${observerClass}`} style={{transitionDelay: ".2s"}}>Développeur Symfony / React</div>
       </div>
    </section>
 }
